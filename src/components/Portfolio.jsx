d440bc1af35568c79db2006101ac9d1f2672a067
riddsep <ridoseptiawan00@gmail.com>
2024-12-13 21:29:36 +0700

    feat: portfolio section

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,22 +1,22 @@
-const Portfolio = () => {
-  return (
-    <section className="px-5 lg:w-[90%] mx-auto my-16 lg:my-[120px] grid grid-cols-1 md:grid-cols-2 gap-10">
-      <div>
-        <h1 className="text-clamp-3 mb-10 font-semibold">Portfolio Kami</h1>
-        <h3 className="font-poppins-600 text-[#F27121] lg:text-xl mb-2">
-          Website Halo Legal
-        </h3>
-        <p className="lg:text-xl">
-          Kami telah membantu Halo Legal membuat website profesional untuk
-          menampilkan layanan legalitas dan perizinan terpercaya, memperkuat
-          branding, dan mempermudah komunikasi dengan para pelaku usaha.
-        </p>
-      </div>
-      <div>
-        <img src="/portfolio.svg" />
-      </div>
-    </section>
-  );
-};
+// const Portfolio = () => {
+//   return (
+//     <section className="px-5 lg:w-[90%] mx-auto my-16 lg:my-[120px] grid grid-cols-1 md:grid-cols-2 gap-10">
+//       <div>
+//         <h1 className="text-clamp-3 mb-10 font-semibold">Portfolio Kami</h1>
+//         <h3 className="font-poppins-600 text-[#F27121] lg:text-xl mb-2">
+//           Website Halo Legal
+//         </h3>
+//         <p className="lg:text-xl">
+//           Kami telah membantu Halo Legal membuat website profesional untuk
+//           menampilkan layanan legalitas dan perizinan terpercaya, memperkuat
+//           branding, dan mempermudah komunikasi dengan para pelaku usaha.
+//         </p>
+//       </div>
+//       <div>
+//         <img src="/portfolio.svg" />
+//       </div>
+//     </section>
+//   );
+// };
 
-export default Portfolio;
+// export default Portfolio;
