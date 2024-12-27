a50a73dff3e691e32a544d549936c166cca33f49
riddsep <ridoseptiawan00@gmail.com>
2024-12-27 10:47:46 +0700

    minor changes

---
+++ src/components/Konsultasi.jsx
@@ -0,0 +1,11 @@
+function Konsultasi() {
+  return (
+    <div className="px-5 lg:w-[90%] mx-auto my-16 lg:my-[120px]">
+      <h1 className="text-clamp-3 mb-10 font-semibold">
+        Konsultasikan bisnis kamu sekarang
+      </h1>
+    </div>
+  );
+}
+
+export default Konsultasi;
